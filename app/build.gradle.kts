20a6bd48ccb89a9c8067cc6906bdb9b986969c3c
yanyannoodle34-debug <yanyannoodle34@gmail.com>
2026-08-02 22:09:29 +0630

    Final1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,11 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
-
+  compileSdk = 35
   defaultConfig {
     applicationId = "com.aistudio.aetheros.orch"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
